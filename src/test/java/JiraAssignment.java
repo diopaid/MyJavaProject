30dc6160a5dfc1349a980997a658428da81f207c
diopaid <diopaid@gmail.com>
2023-05-29 12:49:39 -0500

    first commit

---
+++ src/test/java/JiraAssignment.java
@@ -0,0 +1,19 @@
+public class JiraAssignment {
+
+
+//    public static void main (String [] args){
+//
+//        System.out.println(System.getProperty("user.dir"));
+//    }
+//}
+
+        public static void main(String[] args) {
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+
+            }
+        }
+
+
+}
